bce1749eb042e56d77563e447bda45be4b149c77
vipul <vipuljain906@gmail.com>
2020-06-19 19:34:40 +0530

    Updates

---
+++ precache-manifest.25e9cba01acfeb73085e6db71391aedf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963ea7b0af068c3bf2fc8e3bf5b6f912",
+    "url": "/shopingcart/index.html"
+  },
+  {
+    "revision": "9c4431f19211ece0e89b",
+    "url": "/shopingcart/static/css/main.d1552f98.chunk.css"
+  },
+  {
+    "revision": "598ef51a97798ad73d51",
+    "url": "/shopingcart/static/js/2.03a6a12f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/shopingcart/static/js/2.03a6a12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4431f19211ece0e89b",
+    "url": "/shopingcart/static/js/main.da2bcb76.chunk.js"
+  },
+  {
+    "revision": "fa6671d02436fd60f7ca",
+    "url": "/shopingcart/static/js/runtime-main.92d99acc.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/shopingcart/static/media/101_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/shopingcart/static/media/101_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/shopingcart/static/media/102_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/shopingcart/static/media/103_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/shopingcart/static/media/104_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/shopingcart/static/media/105_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/shopingcart/static/media/106_1.87b5353e.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/shopingcart/static/media/107_1.59b64365.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/shopingcart/static/media/108_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/shopingcart/static/media/109_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/shopingcart/static/media/110_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/shopingcart/static/media/111_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/shopingcart/static/media/112_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/shopingcart/static/media/113_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/shopingcart/static/media/114_1.2995d79a.jpg"
+  },
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/shopingcart/static/media/115_1.d78e2790.jpg"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/shopingcart/static/media/116_1.75f51cec.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/shopingcart/static/media/117_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/shopingcart/static/media/118_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/shopingcart/static/media/119_1.df947f14.jpg"
+  },
+  {
+    "revision": "afce9b2fed0822dd156cae0c37acec3f",
+    "url": "/shopingcart/static/media/cart.afce9b2f.png"
+  }
+]);
